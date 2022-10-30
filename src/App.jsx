903d210b43458d5863927041a7e2568b9760dd8f
unknown <dikshantsingh510@gmail.com>
2022-10-30 19:37:23 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
     <div className='w-full h-full bg-slate-900 flex justify-center py-2 overflow-hidden
     relative'>
-      <div className=""></div>
+      
       <div className="rounded sm:columns-2 md:columns-3 xl:columns-4 gap-2 w-11/12 box-border">
         {Data ? (Data.map((item, index) => { return (<Image url={item} key={item.id} />) })) : "NoT images found"}
       </div>
